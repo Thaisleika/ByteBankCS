f92985815ab8fca5bb2719797381f265c9277c87
THAIS LEIKA <thaisleika@outlook.com>
2023-01-05 21:50:09 -0300

    Update Program.cs

--- ByteBankCS/ByteBankCS/Program.cs
+++ ByteBankCS/ByteBankCS/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Drawing;
 using System.Reflection.Metadata.Ecma335;
-using Internal;
 
 namespace ByteBank1
 {
@@ -10,12 +10,12 @@ namespace ByteBank1
 
         static void ShowMenu()
         {
-            Console.WriteLine("1 - Inserir novo usuário");
-            Console.WriteLine("2 - Deletar um usuário");
-            Console.WriteLine("3 - Listar todas as contas registradas");
+            Console.WriteLine("1 - Cadastrar novo usuário");
+            Console.WriteLine("2 - Deletar usuário");
+            Console.WriteLine("3 - Listar contas registradas");
             Console.WriteLine("4 - Detalhes de um usuário");
             Console.WriteLine("5 - Quantia armazenada no banco");
-            Console.WriteLine("6 - Manipular a conta");
+            Console.WriteLine("6 - Transações");
             Console.WriteLine("0 - Para sair do programa");
             Console.Write("Digite a opção desejada: ");
         }
@@ -28,7 +28,7 @@ namespace ByteBank1
             titulares.Add(Console.ReadLine());
             Console.Write("Digite a senha: ");
             senhas.Add(Console.ReadLine());
-            saldos.Add(0);
+            saldos.Add(0); //aqui saldo começa com 0
         }
 
         static void DeletarUsuario(List<string> cpfs, List<string> titulares, List<string> senhas, List<double> saldos)
@@ -76,14 +76,123 @@ namespace ByteBank1
 
         static void ApresentarValorAcumulado(List<double> saldos)
         {
-            Console.WriteLine($"Total acumulado no banco: {saldos.Sum()}");
-            // saldos.Sum(); ou .Agregatte(0.0, (x, y) => x + y)
+            Console.WriteLine($"Total acumulado no banco: {saldos.Sum():F2}");
         }
 
         static void ApresentaConta(int index, List<string> cpfs, List<string> titulares, List<double> saldos)
         {
             Console.WriteLine($"CPF = {cpfs[index]} | Titular = {titulares[index]} | Saldo = R${saldos[index]:F2}");
         }
+
+     
+
+        static void Transacoes(List<string> cpfs, List<string> titulares, List<string> senhas, List<double> saldos)
+        {
+            Console.Write("Digite seu CPF:");
+            string cpfParaLoggin = (Console.ReadLine());
+            int indexParaLoggin = cpfs.FindIndex(cpf => cpf == cpfParaLoggin);// achei o index daquele cpf
+
+            Console.Write("Digite sua senha:");
+            string senhaParaLoggin = (Console.ReadLine());
+
+            if (indexParaLoggin == -1)
+            {
+                Console.WriteLine("Não foi possível deletar esta Conta");
+                Console.WriteLine("MOTIVO: Conta não encontrada.");
+                Console.Write("Digite seu CPF novamente:");
+                cpfParaLoggin = (Console.ReadLine());
+            }
+
+
+            else if (senhaParaLoggin != senhas[indexParaLoggin])
+            {
+                Console.WriteLine("Senha invalida");
+                Console.Write("Digite sua senha novamente:");
+                senhaParaLoggin = (Console.ReadLine());
+            }
+            else 
+ 
+            Console.WriteLine("Qual opçao abaixo você deseja realizar?");
+
+            static void ShowMenuTransacoes()
+            {
+                Console.WriteLine("0 - Sair/Loggout");
+                Console.WriteLine("1 - Transferência");
+                Console.WriteLine("2 - Saque");
+                Console.WriteLine("3 - Depósito");
+                Console.Write("Digite a opção desejada: ");
+            }
+
+          int option;
+            do
+            {
+                ShowMenuTransacoes();
+                option = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("-----------------");
+
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("Voltar ao Menu inicial...");
+                        break;
+                    case 1:
+                        RealizarTransferencia(cpfs, titulares, saldos);
+                        break;
+                    case 2:
+                        RealizarSaque(cpfs,titulares,saldos);
+                        break;
+                    case 3:
+                        RealizarDeposito(cpfs, titulares, saldos);
+                        break;
+                }
+          
+            static void RealizarTransferencia(List<string> cpfs, List<string> titulares, List<double> saldos)
+            {
+                Console.WriteLine("Valor a ser transferido");
+                double valorAserTransferido = double.Parse(Console.ReadLine());
+                Console.Write("Digite o cpf: ");
+                string cpfParaTransferencia = Console.ReadLine();
+                int indexParaTransacao = cpfs.FindIndex(cpf => cpf == cpfParaTransferencia);
+                saldos[indexParaTransacao] -= valorAserTransferido;
+
+                Console.Write("Digite o cpf para quem deseja transferir: ");
+                string cpfParaTransferir = Console.ReadLine();
+                int indexParaTransferir = cpfs.FindIndex(cpf => cpf == cpfParaTransferir);
+                saldos[indexParaTransferir] += valorAserTransferido;
+
+                Console.WriteLine($"Saldo Atual: {saldos[indexParaTransacao]:F2}");
+            };
+
+            static void RealizarSaque(List<string> cpfs, List<string> titulares, List<double> saldos)
+            {
+                    Console.WriteLine("Valor a ser sacado");
+                    double valorAserSacado = double.Parse(Console.ReadLine());
+                    Console.Write("Digite o cpf: ");
+                    string cpfParaTransferencia = Console.ReadLine();
+                    int indexParaTransacao = cpfs.FindIndex(cpf => cpf == cpfParaTransferencia);
+                    saldos[indexParaTransacao] -= valorAserSacado;
+
+                    Console.WriteLine($"Saldo Atual: {saldos[indexParaTransacao]:F2}");
+            };
+  
+            static void RealizarDeposito(List<string> cpfs, List<string> titulares, List<double> saldos)
+                {
+                    Console.WriteLine("Valor a ser depositado");
+                    double valorAserDepositado = double.Parse(Console.ReadLine());
+                    Console.Write("Digite o cpf: ");
+                    string cpfParaTransferencia = Console.ReadLine();
+                    int indexParaTransacao = cpfs.FindIndex(cpf => cpf == cpfParaTransferencia);
+                    saldos[indexParaTransacao] += valorAserDepositado;
+
+                    Console.WriteLine($"Saldo Atual: {saldos[indexParaTransacao]:F2}");
+                }
+
+              Console.WriteLine("-----------------");
+
+            } while (option != 0);
+            
+    }
 
         public static void Main(string[] args)
         {
@@ -121,6 +230,12 @@ namespace ByteBank1
                     case 4:
                         ApresentarUsuario(cpfs, titulares, saldos);
                         break;
+                    case 5:
+                        ApresentarValorAcumulado(saldos);
+                        break;
+                    case 6:
+                        Transacoes(cpfs, titulares, senhas, saldos);
+                        break;    
                 }
 
                 Console.WriteLine("-----------------");
@@ -128,10 +243,12 @@ namespace ByteBank1
             } while (option != 0);
 
 
-
         }
 
+        private static void Loggin(List<string> cpfs, List<string> titulares, List<string> senhas)
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
-
